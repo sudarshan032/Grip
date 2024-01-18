d69e2626f932cbac4f3dbd3acb8558d576475cae
G SUDARSHAN SASTRY <103668323+sudarshan032@users.noreply.github.com>
2024-01-18 08:38:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
 # Load deliveries dataset
 deliveries_df = pd.read_csv('/content/deliveries.csv')
@@ -151,3 +152,186 @@ plt.xlabel('Average')
 plt.ylabel('Frequency')
 plt.legend()
 plt.show()
+
+# Extracting relevant columns for analysis
+season_deliveries_df = matches_df[['id', 'season', 'winner']]
+complete_deliveries_df = deliveries_df.merge(season_deliveries_df, how='inner', left_on='match_id', right_on='id')
+matches_df = matches_df.drop(columns=["umpire3"], axis=1)
+
+# Number of Matches played in each IPL season
+plt.figure(figsize=(18, 10))
+sns.countplot(x='season', data=matches_df, palette="winter")
+plt.title("Number of Matches played in each IPL season", fontsize=20)
+plt.xlabel("season", fontsize=15)
+plt.ylabel('Matches', fontsize=15)
+plt.show()
+
+# Numbers of matches won by team
+plt.figure(figsize=(18, 10))
+sns.countplot(x='winner', data=matches_df, palette='cool')
+plt.title("Numbers of matches won by team", fontsize=20)
+plt.xticks(rotation=50)
+plt.xlabel("Teams", fontsize=15)
+plt.ylabel("No of wins", fontsize=15)
+plt.show()
+
+# Match Result Pie Chart
+matches_df['win_by'] = np.where(matches_df['win_by_runs'] > 0, 'Bat first', 'Bowl first')
+Win = matches_df.win_by.value_counts()
+labels = np.array(Win.index)
+sizes = Win.values
+colors = ['#FFBF00', '#FA8072']
+plt.figure(figsize=(10, 8))
+plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=90)
+plt.title('Match Result', fontsize=20)
+plt.gca().set_aspect('equal')  # Equal aspect ratio ensures that pie is drawn as a circle
+plt.tick_params(labelsize=10)
+plt.show()
+
+# Numbers of matches won by batting and bowling first
+plt.figure(figsize=(18, 10))
+sns.countplot(x='season', hue='win_by', data=matches_df, palette='hsv')
+plt.title("Numbers of matches won by batting and bowling first", fontsize=20)
+plt.xlabel("Season", fontsize=15)
+plt.ylabel("Count", fontsize=15)
+plt.show()
+
+# Toss Decision Pie Chart
+Toss = matches_df.toss_decision.value_counts()
+labels = np.array(Toss.index)
+sizes = Toss.values
+colors = ['#FFBF00', '#FA8072']
+plt.figure(figsize=(10, 8))
+plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=90)
+plt.title('Toss Decision', fontsize=20)
+plt.gca().set_aspect('equal')  # Equal aspect ratio ensures that pie is drawn as a circle
+plt.tick_params(labelsize=10)
+plt.show()
+
+# Numbers of matches won by Toss result
+plt.figure(figsize=(18, 10))
+sns.countplot(x='season', hue='toss_decision', data=matches_df, palette='afmhot')
+plt.title("Numbers of matches won by Toss result", fontsize=20)
+plt.xlabel("Season", fontsize=15)
+plt.ylabel("Count", fontsize=15)
+plt.show()
+
+# Winner season wise
+final_matches = matches_df.drop_duplicates(subset=['season'], keep='last')
+final_matches[['season', 'winner']].reset_index(drop=True).sort_values('season')
+
+# Winning percentage in final Pie Chart
+match = final_matches.win_by.value_counts()
+labels = np.array(Toss.index)
+sizes = match.values
+colors = ['gold', 'purple']
+plt.figure(figsize=(10, 8))
+plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=90)
+plt.title('Winning percentage in final', fontsize=20)
+plt.gca().set_aspect('equal')  # Equal aspect ratio ensures that pie is drawn as a circle
+plt.tick_params(labelsize=10)
+plt.show()
+
+# Toss Result Pie Chart
+Toss = final_matches.toss_decision.value_counts()
+labels = np.array(Toss.index)
+sizes = Toss.values
+colors = ['#FFBF00', '#FA8072']
+plt.figure(figsize=(10, 8))
+plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=90)
+plt.title('Toss Result', fontsize=20)
+plt.gca().set_aspect('equal')  # Equal aspect ratio ensures that pie is drawn as a circle
+plt.tick_params(labelsize=10)
+plt.show()
+
+# Top player of the match Winners
+plt.figure(figsize=(18, 10))
+top_players = matches_df.player_of_match.value_counts()[:10]
+fig, ax = plt.subplots()
+ax.set_ylim([0, 20])
+ax.set_ylabel("Count")
+ax.set_title("Top player of the match Winners")
+top_players.plot.bar()
+sns.barplot(x=top_players.index, y=top_players, orient='v', palette="hsv")
+plt.show()
+
+# IPL Finals venues and winners along with the number of wins
+final_matches.groupby(['city', 'winner']).size()
+
+# Number of seasons won by teams
+final_matches["winner"].value_counts()
+
+# Toss winner, toss decision, winner in final matches
+final_matches[['toss_winner', 'toss_decision', 'winner']].reset_index(drop=True)
+
+# Man of the match
+final_matches[['winner', 'player_of_match']].reset_index(drop=True)
+
+# Number of fours hit by team
+four_deliveries_df = complete_deliveries_df[complete_deliveries_df['batsman_runs'] == 4]
+fours_by_team = four_deliveries_df.groupby('batting_team')['batsman_runs'].agg([('runs by fours', 'sum'), ('fours', 'count')])
+
+# Graph on four hits by players
+batsman_four = four_deliveries_df.groupby('batsman')['batsman_runs'].agg([('four', 'count')]).reset_index().sort_values('four', ascending=0)
+ax = batsman_four.iloc[:10, :].plot('batsman', 'four', kind='bar', color='black')
+plt.title("Numbers of fours hit by players", fontsize=20)
+plt.xticks(rotation=50)
+plt.xlabel("Player name", fontsize=15)
+plt.ylabel("No of fours", fontsize=15)
+plt.show()
+
+# Graph on number of fours hit in each season
+ax = four_deliveries_df.groupby('season')['batsman_runs'].agg([('four', 'count')]).reset_index().plot('season', 'four', kind='bar', color='red')
+plt.title("Numbers of fours hit in each season", fontsize=20)
+plt.xticks(rotation=50)
+plt.xlabel("Season", fontsize=15)
+plt.ylabel("No of fours", fontsize=15)
+plt.show()
+
+# Number of sixes hit by team
+six_deliveries_df = complete_deliveries_df[complete_deliveries_df['batsman_runs'] == 6]
+sixes_by_team = six_deliveries_df.groupby('batting_team')['batsman_runs'].agg([('runs by six', 'sum'), ('sixes', 'count')])
+
+# Graph of six hits by players
+batsman_six = six_deliveries_df.groupby('batsman')['batsman_runs'].agg([('six', 'count')]).reset_index().sort_values('six', ascending=0)
+ax = batsman_six.iloc[:10, :].plot('batsman', 'six', kind='bar', color='green')
+plt.title("Numbers of six hit by players", fontsize=20)
+plt.xticks(rotation=50)
+plt.xlabel("Player name", fontsize=15)
+plt.ylabel("No of six", fontsize=15)
+plt.show()
+
+# Graph on number of six hits in each season
+ax = six_deliveries_df.groupby('season')['batsman_runs'].agg([('six', 'count')]).reset_index().plot('season', 'six', kind='bar', color='blue')
+plt.title("Numbers of six hit in each season", fontsize=20)
+plt.xticks(rotation=50)
+plt.xlabel("Season", fontsize=15)
+plt.ylabel("No of six", fontsize=15)
+plt.show()
+
+# Top 10 leading run scorers in IPL
+batsman_score = deliveries_df.groupby('batsman')['batsman_runs'].agg(['sum']).reset_index().sort_values('sum', ascending=False).reset_index(drop=True)
+batsman_score = batsman_score.rename(columns={'sum': 'batsman_runs'})
+print("*** Top 10 Leading Run Scorers in IPL ***")
+batsman_score.iloc[:10, :]
+
+# Number of matches played by batsman
+No_Matches_player = deliveries_df[["match_id", "player_dismissed"]]
+No_Matches_player = No_Matches_player.groupby("player_dismissed")["match_id"].count().reset_index().sort_values(by="match_id", ascending=False).reset_index(drop=True)
+No_Matches_player.columns = ["batsman", "No_of Matches"]
+No_Matches_player.head(5)
+
+# Dismissals in IPL
+plt.figure(figsize=(18, 10))
+ax = sns.countplot(deliveries_df.dismissal_kind)
+plt.title("Dismissals in IPL", fontsize=20)
+plt.xlabel("Dismissals kind", fontsize=15)
+plt.ylabel("count", fontsize=15)
+plt.xticks(rotation=90)
+plt.show()
+
+# IPL most wicket-taking bowlers
+wicket_deliveries_df = deliveries_df.dropna(subset=['dismissal_kind'])
+wicket_deliveries_df = wicket_deliveries_df[~wicket_deliveries_df['dismissal_kind'].isin(['run out', 'retired hurt', 'obstructing the field'])]
+most_wickets = wicket_deliveries_df.groupby('bowler')['dismissal_kind'].agg(['count']).reset_index().sort_values('count', ascending=False).reset_index(drop=True).iloc[:10, :]
+
